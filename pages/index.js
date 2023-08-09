b5b1dd0c5a0f0454c5f858057c6cbee52d99386b
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-08-09 14:49:40 +0300

    useState hook

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react"
+
+
+const InputElement = () => {
+    const [inputText, setinputText] = useState("G");
+    return <div><input
+        onChange={(e) => {
+            setinputText(e.target.value)
+        }}
+        placeholder="Enter your name"/><br/>
+        {inputText}
+        </div>
+};
+
+export default InputElement
